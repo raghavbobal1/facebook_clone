28ef07745e496a7b7435a2c7d13231776c9b1d30
Raghav Bobal <raghavbobal001@gmail.com>
2021-11-01 17:24:18 -0400

    Initial Commit

---
+++ fb_clone/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './Header.js';
+import Sidebar from './Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+      <div className="app__body">
+        <Sidebar/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ fb_clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDzUw2-dM0giu5DSfgwCRfs0vLZhXdsS1M",
+    authDomain: "dynamic-medical.firebaseapp.com",
+    projectId: "dynamic-medical",
+    storageBucket: "dynamic-medical.appspot.com",
+    messagingSenderId: "1059664029811",
+    appId: "1:1059664029811:web:94870b6f16b8965a59e206",
+    measurementId: "G-ZQREZRVCBK"
+  };
